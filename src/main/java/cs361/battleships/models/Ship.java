20371c7da5d6a6a564eee72af17057883c691907
Prathyoosha <chayap@oregonstate.edu>
2019-01-22 17:12:06 -0800

    Add Ship class and Ship tests

--- src/main/java/cs361/battleships/models/Ship.java
+++ src/main/java/cs361/battleships/models/Ship.java
@@ -8,17 +8,53 @@ import java.util.List;
 public class Ship {
 
 	@JsonProperty private List<Square> occupiedSquares;
+	private String kind;
+	private int health;
 
 	public Ship() {
 		occupiedSquares = new ArrayList<>();
+		this.health = 0;
 	}
-	
+
 	public Ship(String kind) {
 		//TODO implement
+		this.kind = kind;
+		this.health = getSize();
+	}
+
+	public String getKind() {
+		return this.kind;
 	}
 
 	public List<Square> getOccupiedSquares() {
 		//TODO implement
-		return null;
+		return this.occupiedSquares;
+	}
+
+	public void setOccupiedSquares(List<Square> occupiedSquares) {
+		this.occupiedSquares = occupiedSquares;
+	}
+
+	public int getSize() {
+		switch (kind) {
+			case "MINESWEEPER":
+				return 2;
+
+			case "DESTROYER":
+				return 3;
+
+			case "BATTLESHIP":
+				return 4;
+		}
+
+		return -1;
+	}
+
+	public void hitShip() {
+		this.health -= 1;
+	}
+
+	public boolean isDead() {
+		return this.health <= 0;
 	}
 }
